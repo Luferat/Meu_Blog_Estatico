c6879a5834a83e82e4b922ff6f5e164b0b8fe902
André Luferat <contato@luferat.net>
2025-01-29 18:06:39 -0300

    aula 09

--- js/config.js
+++ js/config.js
@@ -1,34 +1,47 @@
 /**
+ * Chaves de conexão ccom o Firebase.
+ * ATENÇÃO! Sempre troque essas chaves pelas chaves do seu projeto.
+ **/
+const firebaseConfig = {
+  apiKey: "AIzaSyB_t5qOcHIpgf-z7JwyBkn1mAqxIKtgKiQ",
+  authDomain: "meu-blog-estatico-27dd0.firebaseapp.com",
+  projectId: "meu-blog-estatico-27dd0",
+  storageBucket: "meu-blog-estatico-27dd0.firebasestorage.app",
+  messagingSenderId: "133411976288",
+  appId: "1:133411976288:web:141ec040a4197bad167f05"
+};
+
+/**
  * Configuração do site;
  **/
 const site = {
 
-    /**
-      * Nome do site usado na tag <title>...</title> e nas interações dinâmicas
-      **/
-    nome: "Meu Blog Estático",
+  /**
+   * Nome do site usado na tag <title>...</title> e nas interações dinâmicas
+   **/
+  nome: "Meu Blog Estático",
 
-    /**
-     * Logotipo do site, usado na tag <header>...</title>
-     */
-    logo: "img/logo.png",
+  /**
+   * Logotipo do site, usado na tag <header>...</title>
+   */
+  logo: "img/logo.png",
 
-    /**
-     * Controla a ação ao clicar no link do usuário logado no menu principal
-     * Se `true`, exibe o perfil do usuário → perfil.html
-     * Se `false`, faz logout direto
-     **/
-    verPerfil: true,
+  /**
+   * Controla a ação ao clicar no link do usuário logado no menu principal
+   * Se `true`, exibe o perfil do usuário → perfil.html
+   * Se `false`, faz logout direto
+   **/
+  verPerfil: true,
 
-    /**
-     * Ano de lançamento do site
-     **/
-    ano: 2025,
+  /**
+   * Ano de lançamento do site
+   **/
+  ano: 2025,
 
-    /**
-     * Licensa do site usada no rodapé
-     **/
-    licensa: `
+  /**
+   * Licensa do site usada no rodapé
+   **/
+  licensa: `
     <i class="fa-regular fa-copyright fa-rotate-180 fa-fw"></i>
     <span>Copyleft <span id="footerAno"></span> Joca da Silva</span>
     `,
