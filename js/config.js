8a4cc38ebd8f5ab2b305da6f31af0deb315672d3
André Luferat <contato@luferat.net>
2025-01-29 18:12:54 -0300

    Ajustes da aula 09

--- js/config.js
+++ js/config.js
@@ -1,17 +1,4 @@
 /**
- * Chaves de conexão ccom o Firebase.
- * ATENÇÃO! Sempre troque essas chaves pelas chaves do seu projeto.
- **/
-const firebaseConfig = {
-  apiKey: "AIzaSyB_t5qOcHIpgf-z7JwyBkn1mAqxIKtgKiQ",
-  authDomain: "meu-blog-estatico-27dd0.firebaseapp.com",
-  projectId: "meu-blog-estatico-27dd0",
-  storageBucket: "meu-blog-estatico-27dd0.firebasestorage.app",
-  messagingSenderId: "133411976288",
-  appId: "1:133411976288:web:141ec040a4197bad167f05"
-};
-
-/**
  * Configuração do site;
  **/
 const site = {
